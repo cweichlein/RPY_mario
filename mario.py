5ae2f28e18787d86d2818f65e255bdf8cb74c731
Kit <cweichlein@gmail.com>
2019-10-16 19:39:16 -0700

    mario.py 2

--- mario.py
+++ mario.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.sprite import Sprite
+import math
 
 
 class Mario(Sprite):
@@ -9,3 +10,28 @@ class Mario(Sprite):
         self.screen = screen
         self.stats = stats
         self.scoreboard = scoreboard
+
+        self.screen_rect = screen.get_rect()  # gets the passed screen object's rectangle
+        # self.image = IMPLEMENT!
+        self.rect = self.image.get_rect()  # gets the image's rectangle
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+        self.center = float(self.rect.centerx)
+        self.moving_right = False
+        self.moving_left = False
+        self.die = False
+
+    def update(self):
+        if not self.die:
+            if self.moving_right and self.rect.right < self.screen_rect.right:
+                self.center += self.ai_settings.ship_speed_factor
+            if self.moving_left and self.rect.left > 0:
+                self.center -= self.ai_settings.ship_speed_factor
+            self.rect.centerx = math.floor(self.center)
+
+    # draws mario image at the current position of self.rect
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        self.center = self.screen_rect.centerx
