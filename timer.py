6a424b01ad9200ba4448b15fed531a848d14811d
Kit <cweichlein@gmail.com>
2019-10-16 20:12:16 -0700

    timer.py 2

--- timer.py
+++ timer.py
@@ -1,33 +1,39 @@
 import pygame
+# implement delta time?
+
 
 class Timer:
-    def __init__(self, frames, wait=100, frameindex=0, step=1, looponce=False):    # imagerect frames
+    def __init__(self, frames, wait=100, frame_index=0, step=1, loop_once=False):    # imagerect frames
         self.frames = frames
         self.wait = wait
-        self.frameindex = frameindex
+        self.frame_index = frame_index
         self.step = step
-        self.looponce = looponce
+        self.loop_once = loop_once
         self.finished = False
-        self.lastframe = len(frames) - 1 if step == 1 else 0
+        self.last_frame = len(frames) - 1 if step == 1 else 0
         self.last = None
+
     def frame_index(self):
         now = pygame.time.get_ticks()
         if self.last is None:
             self.last = now
-            self.frameindex = 0 if self.step == 1 else len(self.frames) - 1
+            self.frame_index = 0 if self.step == 1 else len(self.frames) - 1
             return 0
         elif not self.finished and now - self.last > self.wait:
-            self.frameindex += self.step
-            if self.looponce and self.frameindex == self.lastframe:
+            self.frame_index += self.step
+            if self.loop_once and self.frame_index == self.last_frame:
                 self.finished = True
             else:
-                self.frameindex %= len(self.frames)
+                self.frame_index %= len(self.frames)
             self.last = now
-        return self.frameindex
+        return self.frame_index
+
     def reset(self):
         self.last = None
         self.finished = False
+
     def __str__(self): return 'Timer(frames=' + self.frames +\
-                              ', wait=' + str(self.wait) + ', index=' + str(self.frameindex) + ')'
-    def imagerect(self):
+                              ', wait=' + str(self.wait) + ', index=' + str(self.frame_index) + ')'
+
+    def image_rect(self):
         return self.frames[self.frame_index()]
