0852760995aef0569aa5c1e5c0c8165a497d9853
Corey <coreystock@gmail.com>
2019-10-25 20:00:16 -0700

    Finished Mario walk right and walk left animations

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame, sys
 from pygame.time import *
-from block import brick,bricks, pipe
-# from pygame.sprite import Group
 from pygame.math import Vector2
+
+from block import brick,bricks, pipe
 from settings import Settings
+from mario import Mario
 
 
 class main:
@@ -11,13 +12,14 @@ class main:
     def __init__(self):
         pygame.init()
 
-        self.disp = pygame.display.set_mode((500,500))
+        self.disp = pygame.display.set_mode((1000,1000))
 
         self.settings = Settings()
         self.myBrick1 = bricks(self.settings, Vector2(0, 0), 10, self.disp, True)
         self.myBrick2 = bricks(self.settings, Vector2(0, 1), 10, self.disp, False)
         self.Pipe_top = pipe(self.settings, Vector2(1, 1), Vector2(2, 15), self.disp, True, True, False)
         self.pipe_bot = pipe(self.settings, Vector2(3, 1), Vector2(2, 15), self.disp, True, False, False)
+        self.mario = Mario(screen=self.disp, settings=self.settings)
 
     def check_event(self):
         for event in pygame.event.get():
@@ -26,13 +28,17 @@ class main:
 
     def run_game(self):
         while True: # main game loop
+            self.disp.fill((0, 255, 255))
             self.check_event()
             self.myBrick1.draw()
             self.myBrick2.draw()
             self.Pipe_top.draw()
             self.pipe_bot.draw()
+            self.mario.update()
 
-            pygame.display.update()
+            self.mario.blitme()
+
+            pygame.display.flip()
             pygame.time.wait(1)
 
 
--- mario.py
+++ mario.py
@@ -2,24 +2,44 @@ import pygame
 from pygame.sprite import Sprite
 import math
 
+from timer import Timer
+
 
 class Mario(Sprite):
-    def __init__(self, settings, screen, stats, scoreboard):
-        super(Mario, self).__init__(settings, screen, stats, scoreboard)  # initialize superclass object
+    def __init__(self, screen, settings):
+        super(Mario, self).__init__()  # initialize superclass object
         self.settings = settings  # save passed objects for local access
         self.screen = screen
-        self.stats = stats
-        self.scoreboard = scoreboard
+
+
+        self.base_frame = None
+        self.walk_right_frames = None
+        self.walk_left_frames = None
+        self.image = None
+        self.prepare_animation()
 
         self.screen_rect = screen.get_rect()  # gets the passed screen object's rectangle
-        # self.image = IMPLEMENT!
         self.rect = self.image.get_rect()  # gets the image's rectangle
         self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_rect.bottom
+        self.rect.bottom = self.screen_rect.centery
         self.center = float(self.rect.centerx)
         self.moving_right = False
         self.moving_left = False
         self.die = False
+
+        self.timer = Timer(frames=self.base_frame)
+
+    def prepare_animation(self):
+        self.base_frame = [pygame.image.load('image/mario/base/right.png')]
+        self.walk_right_frames = [pygame.image.load('image/mario/walk_right/{}.png'.format(i)) for i in range(0,4)]
+        self.walk_left_frames = [pygame.image.load('image/mario/walk_left/{}.png'.format(i)) for i in range(0,4)]
+        self.image = self.base_frame[0]
+
+    def walk_right(self):
+        self.timer.frames = self.walk_right_frames
+
+    def walk_left(self):
+        self.timer.frames = self.walk_left_frames
 
     def update(self):
         if not self.die:
@@ -28,6 +48,10 @@ class Mario(Sprite):
             if self.moving_left and self.rect.left > 0:
                 self.center -= self.ai_settings.ship_speed_factor
             self.rect.centerx = math.floor(self.center)
+
+        self.walk_left()
+        self.image = self.timer.image_rect()
+
 
     # draws mario image at the current position of self.rect
     def blitme(self):
