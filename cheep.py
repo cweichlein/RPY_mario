e63533f7a3a2c85a9a4e51cc65bbbc7e625ffbd2
jongster3908 <50344489+jongster3908@users.noreply.github.com>
2019-10-28 17:19:05 -0700

    adding enemies, specifically goombas and green troopas

---
+++ cheep.py
@@ -0,0 +1,7 @@
+import pygame
+import math
+
+from animation import Animation
+from enemy import Enemy
+from settings import Settings
+from timer import Timer
---
+++ paratroopa.py
@@ -0,0 +1,6 @@
+import pygame
+import math
+
+from animation import Animation
+from enemy import Enemy
+from timer import Timer
